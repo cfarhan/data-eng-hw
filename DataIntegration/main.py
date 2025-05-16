a077caa9dea9f403e4567ae1dd1dfbae9bbc37c9
cfarhan <cameronfarhan2@gmail.com>
2025-05-16 13:40:01 -0700

    Initial setup for DataIntegration

---
+++ DataIntegration/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+cases_df = pd.read_csv('covid_confirmed_usafacts.csv')
+deaths_df = pd.read_csv('covid_deaths_usafacts.csv')
+census_df = pd.read_csv('acs2017_county_data.csv')
+
